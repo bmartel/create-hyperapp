20c952f35323e0ac330058fd15a301c7d767676e
Brandon Martel <brandonmartel@gmail.com>
2020-08-22 16:07:33 -0500

    forgot to use hyperload

--- template/src/index.js
+++ template/src/index.js
@@ -1,12 +1,14 @@
 import {h, app} from "hyperapp";
 import {Router} from "hyperapp-page-router";
+import {withHyperload} from "@martel/hyperload"
+import './routes';
 import "./index.output.css";
 import App from "./App";
 import * as serviceWorker from "./sw";
 
 const initialState = {title: "Learn Hyperapp"};
 
-app({
+withHyperload(app)({
   init: () => initialState,
   view: App,
   subscriptions: () => [
